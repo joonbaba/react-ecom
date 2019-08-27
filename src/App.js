aca405370736490959456f0a27946c1e691f67d9
Amir Jannatpour <jannatpour@hotmail.com>
2019-08-27 10:50:53 -0700

    update our menuitem

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import './App.css';
 
+import
+{
+	Route
+}
+from 'react-router-dom'
+
+
 import HomePage from './pages/homepage/homepage.component';
 
-function App() {
-  return (
-    <div>
-      <HomePage />
+const HatsPage = () => (
+	<div>
+<h1> HATS PAGE </h1>
+  </div>
+)
+
+function App()
+{
+	return (
+		<div>
+      <Route exact path='/' component={HomePage} />
+      <Route  path='/hats' component={HatsPage} />
     </div>
-  );
+	);
 }
 
 export default App;
